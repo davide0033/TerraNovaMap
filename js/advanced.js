57c03fea42198b6af15f318a31fa004d5a897040
Davide0033 <70667149+davide0033@users.noreply.github.com>
2024-12-09 16:21:09 +0100

    alliances + quality of live update
    added alliance info in the town overview, updated the town resident list so now it scrolls instead of taking up the whole screen, added a search option under the "advanced features" menù (still a work in progress)

---
+++ js/advanced.js
@@ -0,0 +1,48 @@
+const Snput = document.getElementById("search")
+const type = document.getElementById("SeachSelect")
+
+function getNation(desc) {
+	const nationWikiRegex = /(?<=nofollow">)[^<]+(?=<\/a>\))/;
+	const nationRegex = /(?<=\()[^)]*/;
+	if (desc.includes('(<a ')) return desc.match(nationWikiRegex)[0];
+	else return desc.match(nationRegex)[0];
+}
+
+
+
+
+
+Snput.addEventListener("keydown", async (event) => {
+    
+    if (event.key === "Enter") {
+        if (Snput.value.trim() !== "") {
+            const markers = await fetch("./assets/markers_earth.json")
+		.then(resp => resp.json())
+		.then(json => json.sets['townyPlugin.markerset'].areas)
+		.catch(() => {
+			sendAlert(`Couldn't get needed data, try again later.`);
+			return;
+		});
+
+	const town = Object.values(markers).find(town => {
+		const {desc, label} = town;
+		if (type.value === 'nation') {
+			const nation = getNation(desc);
+			return nation.toLowerCase() === Snput.value.toLowerCase() && desc.includes('capital: true');
+		} else return label.toLowerCase() === Snput.value.toLowerCase();
+	});
+	if (town) {
+		const avgX = (Math.min(...town.x) + Math.max(...town.x)) / 2;
+		const avgZ = (Math.min(...town.z) + Math.max(...town.z)) / 2;
+		location.href = `./?y=${-avgZ}&x=${avgX}&zoom=-6`;
+		return;
+	}
+        } 
+    }
+});
+
+
+
+
+
+
